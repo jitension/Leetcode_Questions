f452e2a0331c71ed92e9c003ddd3124ac4485337
Jitendra Purohit <jpurohit@stevens.edu>
2017-10-25 15:38:36 -0400

    Q7 code

---
+++ Q7.java
@@ -0,0 +1,17 @@
+/* 7. Reverse Integer
+Reverse digits of an integer.
+*/
+
+class Solution {
+    public int reverse(int x) {
+        
+        long reversedNum=0;
+        while (x != 0) {    
+           reversedNum = reversedNum * 10 + (x%10);
+           x = x/10; 
+         }
+        if(reversedNum > Integer.MAX_VALUE || reversedNum < Integer.MIN_VALUE)
+         return 0;
+        return (int)(reversedNum);
+    }
+}
