c4ae323dbe2421a38e0232aa90251ffff2bb5d28
Jitendra Purohit <jpurohit@stevens.edu>
2017-10-25 15:43:19 -0400

    Q1 code

---
+++ Q1.java
@@ -0,0 +1,14 @@
+/* 1. Two Sum
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+var twoSum = function(nums, target) {
+    for(let i =0 ;i<nums.length;i++){
+        for(let j= i+1;j<nums.length;j++){
+            if(target === nums[i]+nums[j])
+                return [i,j];
+        }
+    }
+};
